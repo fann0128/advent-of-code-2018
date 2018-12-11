d65d2d400cf071234838a73c43c5549d9f085e4d
fan0128 <fan.yang@osedea.com>
2018-12-11 14:20:10 -0500

    Completed day 6 part 2

--- day 6/6day_part2.py
+++ day 6/6day_part2.py
@@ -22,37 +22,35 @@ map = [[0 for x in range(height + 1)] for y in range(width + 1)]
 for location in locations :
     map[int(location["location"][0])][int(location["location"][1])] = location["location"]
 
-def check_closest_locaiton(unknown_location, locations) :
+def check_close_to_all_locaitons(unknown_location, locations) :
     to_closest_location = 0
     closest_location = ""
-    is_close_to_more_than_one_location = False
-    
+    total_distance_to_all_locations = 0
+
     for location in locations :
         distance = abs(int(location["location"][0]) - int(unknown_location[0])) + abs(int(location["location"][1]) - int(unknown_location[1]))
-        if (to_closest_location == 0 or distance < to_closest_location):
-            to_closest_location = distance
-            closest_location = location
-            is_close_to_more_than_one_location = False
-        elif (to_closest_location == distance) :
-            is_close_to_more_than_one_location = True
-    if (not is_close_to_more_than_one_location) :
-        return closest_location
+        total_distance_to_all_locations += distance
+
+    if (total_distance_to_all_locations < 10000) :
+        return location
     return False
 
 for x in range(width + 1) :
     for y in range(height + 1) :
-        if map[x][y] == 0 :
-            if (check_closest_locaiton([x,y], locations)) :
-                location = check_closest_locaiton([x,y], locations)
-                map[x][y] = location["name"]
-                location["dot_count"] += 1
+            location = check_close_to_all_locaitons([x,y], locations)
+            if (location) :
+                map[x][y] = "#"
                 # check the border, get the locations that extends to border
                 if (x == 0 or x == width or y == 0 or y == height) :
                     location["is_infinite"] = True
             else:
                 map[x][y] = "."
 
-not_infinite_location = list(filter(lambda location: not location["is_infinite"], locations))
-not_infinite_location.sort(key=lambda x: x["dot_count"], reverse=True)
-
-pprint(not_infinite_location)
+# not_infinite_location = list(filter(lambda location: not location["is_infinite"], locations))
+# not_infinite_location.sort(key=lambda x: x["dot_count"], reverse=True)
+count = 0
+for x in range(width + 1) :
+    for y in range(height + 1) :
+        if (map[x][y] == "#") :
+            count += 1
+print (count)
